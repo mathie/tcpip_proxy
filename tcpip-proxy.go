eeeea1c8d2aaa02183a4fdd6cff8467caff9042f
Graeme Mathieson <mathie@woss.name>
2013-05-15 15:24:25 +0100

    Eradicate underscores from main, too.

--- tcpip-proxy.go
+++ tcpip-proxy.go
@@ -15,7 +15,7 @@ import (
 var (
   host *string = flag.String("host", "", "target host or address")
   port *string = flag.String("port", "0", "target port")
-  listen_port *string = flag.String("listen_port", "0", "listen port")
+  listenPort *string = flag.String("listenPort", "0", "listen port")
 )
 
 func warn(format string, v ...interface{}) {
@@ -28,7 +28,7 @@ func die(format string, v ...interface{}) {
 }
 
 
-func printable_addr(a net.Addr) string {
+func printableAddr(a net.Addr) string {
   return strings.Replace(a.String(), ":", "-", -1)
 }
 
@@ -39,14 +39,14 @@ const (
 
 type Channel struct {
   from,   to            net.Conn
-  connectionLogger, binary_logger *logger.Logger
+  connectionLogger, binaryLogger *logger.Logger
   ack                   chan bool
 }
 
 func NewChannel(connection *Connection, direction int, connectionLogger *logger.Logger) *Channel {
   peer := connection.Info(direction)
   binaryLogger := logger.NewBinaryLogger(connection.connectionNumber, peer)
-  channel := &Channel{ from: connection.From(direction), to: connection.To(direction), connectionLogger: connectionLogger, binary_logger: binaryLogger, ack: connection.ack }
+  channel := &Channel{ from: connection.From(direction), to: connection.To(direction), connectionLogger: connectionLogger, binaryLogger: binaryLogger, ack: connection.ack }
 
   go channel.PassThrough()
   return channel
@@ -61,7 +61,7 @@ func (channel Channel) LogHex(bytes []byte) {
 }
 
 func (channel Channel) LogBinary(bytes []byte) {
-  channel.binary_logger.LogBinary(bytes)
+  channel.binaryLogger.LogBinary(bytes)
 }
 
 func (channel Channel) Read(buffer []byte) (n int, err error) {
@@ -76,22 +76,22 @@ func (channel Channel) Disconnect() {
   channel.Log("Disconnected from %s", channel.FromAddr())
   channel.from.Close()
   channel.to.Close()
-  channel.binary_logger.Close()
+  channel.binaryLogger.Close()
   channel.ack <- true
 }
 
 func (channel Channel) FromAddr() (addr string) {
-  return printable_addr(channel.from.LocalAddr())
+  return printableAddr(channel.from.LocalAddr())
 }
 
 func (channel Channel) ToAddr() (addr string) {
-  return printable_addr(channel.to.LocalAddr())
+  return printableAddr(channel.to.LocalAddr())
 }
 
 func (channel Channel) PassThrough() {
   b := make([]byte, 10240)
   offset := 0
-  packet_n := 0
+  packetNumber := 0
 
   for {
     n, err := channel.Read(b)
@@ -103,17 +103,17 @@ func (channel Channel) PassThrough() {
       continue
     }
 
-    channel.Log("Received (#%d, %08X) %d bytes from %s", packet_n, offset, n, channel.FromAddr())
+    channel.Log("Received (#%d, %08X) %d bytes from %s", packetNumber, offset, n, channel.FromAddr())
 
     channel.LogHex(b[:n])
     channel.LogBinary(b[:n])
 
     channel.Write(b[:n])
 
-    channel.Log("Sent (#%d) to %s\n", packet_n, channel.ToAddr())
+    channel.Log("Sent (#%d) to %s\n", packetNumber, channel.ToAddr())
 
     offset += n
-    packet_n += 1
+    packetNumber += 1
   }
 
   channel.Disconnect()
@@ -139,11 +139,11 @@ func NewConnection(local net.Conn, connectionNumber int, target string) *Connect
 }
 
 func (connection Connection) LocalInfo() string {
-  return printable_addr(connection.remote.LocalAddr())
+  return printableAddr(connection.remote.LocalAddr())
 }
 
 func (connection Connection) RemoteInfo() string {
-  return printable_addr(connection.remote.RemoteAddr())
+  return printableAddr(connection.remote.RemoteAddr())
 }
 
 func (connection Connection) Info(direction int) string {
@@ -202,20 +202,20 @@ func (connection Connection) Process() {
 
 type Proxy struct {
   target string
-  local_port string
-  conn_n int
+  localPort string
+  connectionNumber int
 }
 
-func RunProxy(target_host, target_port, local_port string) {
-  target := net.JoinHostPort(target_host, target_port)
-  proxy := &Proxy{ target: target, local_port: local_port, conn_n: 1 }
+func RunProxy(targetHost, targetPort, localPort string) {
+  target := net.JoinHostPort(targetHost, targetPort)
+  proxy := &Proxy{ target: target, localPort: localPort, connectionNumber: 1 }
   proxy.Run()
 }
 
 func (proxy Proxy) Run() {
-  fmt.Printf("Start listening on port %s and forwarding data to %s\n", proxy.local_port, proxy.target)
+  fmt.Printf("Start listening on port %s and forwarding data to %s\n", proxy.localPort, proxy.target)
 
-  ln, err := net.Listen("tcp", ":" + proxy.local_port)
+  ln, err := net.Listen("tcp", ":" + proxy.localPort)
   if err != nil {
     die("Unable to start listener %v", err)
   }
@@ -232,19 +232,19 @@ func (proxy Proxy) Run() {
 }
 
 func (proxy Proxy) ProcessConnection(connection net.Conn) {
-  NewConnection(connection, proxy.conn_n, proxy.target)
+  NewConnection(connection, proxy.connectionNumber, proxy.target)
 
-  proxy.conn_n += 1
+  proxy.connectionNumber += 1
 }
 
 func main() {
   runtime.GOMAXPROCS(runtime.NumCPU())
   flag.Parse()
   if flag.NFlag() != 3 {
-    warn("Usage: tcpip-proxy -host target_host -port target_port -listen_port local_port")
+    warn("Usage: tcpip-proxy -host targetHost -port targetPort -listenPort localPort")
     flag.PrintDefaults()
     os.Exit(1)
   }
 
-  RunProxy(*host, *port, *listen_port)
+  RunProxy(*host, *port, *listenPort)
 }
