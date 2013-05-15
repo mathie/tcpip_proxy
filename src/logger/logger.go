9ced4e00ddf3c5002ab9081d5866efcb918da0f2
Graeme Mathieson <mathie@woss.name>
2013-05-15 15:13:36 +0100

    Separate the public (exported) and private methods.

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -11,26 +11,12 @@ type Logger struct {
   data chan []byte
 }
 
-func NewLogger(filename string) *Logger {
-  logger := &Logger { data: make(chan []byte), filename: filename }
-  go logger.LoggerLoop()
-  return logger
-}
-
-func ConnectionLoggerFilename(conn_n int, local_info, remote_info string) string {
-  return fmt.Sprintf("log-%s-%04d-%s-%s.log", formatTime(time.Now()), conn_n, local_info, remote_info)
-}
-
 func NewConnectionLogger(conn_n int, local_info, remote_info string) *Logger {
-  return NewLogger(ConnectionLoggerFilename(conn_n, local_info, remote_info))
-}
-
-func BinaryLoggerFilename(conn_n int, peer string) string {
-  return fmt.Sprintf("log-binary-%s-%04d-%s.log", formatTime(time.Now()), conn_n, peer)
+  return new(connectionLoggerFilename(conn_n, local_info, remote_info))
 }
 
 func NewBinaryLogger(conn_n int, peer string) *Logger {
-  return NewLogger(BinaryLoggerFilename(conn_n, peer))
+  return new(binaryLoggerFilename(conn_n, peer))
 }
 
 func (logger Logger) Log(format string, v ...interface{}) {
@@ -41,7 +27,25 @@ func(logger Logger) LogBinary(bytes []byte) {
   logger.data <- bytes
 }
 
-func (logger Logger) LoggerLoop() {
+func (logger Logger) Close() {
+  logger.data <- []byte{}
+}
+
+func new(filename string) *Logger {
+  logger := &Logger { data: make(chan []byte), filename: filename }
+  go logger.loggerLoop()
+  return logger
+}
+
+func connectionLoggerFilename(conn_n int, local_info, remote_info string) string {
+  return fmt.Sprintf("log-%s-%04d-%s-%s.log", formatTime(time.Now()), conn_n, local_info, remote_info)
+}
+
+func binaryLoggerFilename(conn_n int, peer string) string {
+  return fmt.Sprintf("log-binary-%s-%04d-%s.log", formatTime(time.Now()), conn_n, peer)
+}
+
+func (logger Logger) loggerLoop() {
   f, err := os.Create(logger.filename)
   if err != nil {
     panic(fmt.Sprintf("Unable to create log file, %s, %v", logger.filename, err))
@@ -60,11 +64,6 @@ func (logger Logger) LoggerLoop() {
   }
 }
 
-func (logger Logger) Close() {
-  logger.data <- []byte{}
-}
-
 func formatTime(t time.Time) string {
   return t.Format("2006.01.02-15.04.05")
 }
-
